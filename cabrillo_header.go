af5d18b2bd4ae7ad643139a816dedc232a98fbd8
Elena Samsonova <samsonova.elena@gmail.com>
2024-01-21 11:01:44 -0800

    Extract askString, add ARRL sections

--- cabrillo_header.go
+++ cabrillo_header.go
@@ -9,11 +9,11 @@ type CodedChoice struct {
 	code string
 }
 
-func askCallsign() string {
-	callSign := ""
-	fmt.Printf("Enter call sign:\n")
-	fmt.Scanf("%s\n", &callSign)
-	return callSign
+func askString(prompt string) string {
+	answerString := ""
+	fmt.Printf("Enter %s:\n", prompt)
+	fmt.Scanf("%s\n", &answerString)
+	return answerString
 }
 
 func askChoiceList(fieldName string, options []string) string {
@@ -38,6 +38,10 @@ func askChoiceCode(fieldName string, options []CodedChoice) string {
 
 }
 
+func askCallsign() string {
+	return askString("callsign")
+}
+
 func askStationType() string {
 	return askChoiceList("category-station", []string{"FIXED", "MOBILE", "SCHOOL"})
 }
@@ -51,7 +55,7 @@ func askAssistedCat() string {
 }
 
 func askLocationCode() string {
-	return askChoiceCode("location", []CodedChoice{{"East Bay", "EB"}, {"Los Angeles", "LAX"}, {"Orange", "ORG"}, {"Santa Barbara", "SB"}, {"Santa Clara Valley", "SCV"}, {"San Diego", "SDG"}, {"San Francisco", "SF"}, {"San Joaquin Valley", "SJV"}, {"Sacramento Valley", "SV"}, {"Pacific", "PAC"}})
+	return askChoiceCode("location", arrl_sections)
 }
 
 func askClass(classTypes []string) string {
@@ -148,4 +152,92 @@ func PrintCabrilloHeader() {
 		NovSweepCW()
 	}
 
+}
+
+var arrl_sections = []CodedChoice{
+	{"Colorado", "CO"},
+	{"Iowa", "IA"},
+	{"Kansas", "KS"},
+	{"Minnesota", "MN"},
+	{"Missouri", "MO"},
+	{"North Dakota", "ND"},
+	{"Nebraska", "NE"},
+	{"South Dakota", "SD"},
+	{"Connecticut", "CT"},
+	{"Eastern Massachusetts", "EMA"},
+	{"Maine", "ME"},
+	{"New Hampshire", "NH"},
+	{"Rhode Island", "RI"},
+	{"Vermont", "VT"},
+	{"Western Massachusetts", "WMA"},
+	{"Eastern New York", "ENY"},
+	{"New York City-Long Island", "NLI"},
+	{"Northern New Jersey", "NNJ"},
+	{"Northern New York", "NNY"},
+	{"Southern New Jersey", "SNJ"},
+	{"Western New York", "WNY"},
+	{"Delaware", "DE"},
+	{"Eastern Pennsylvania", "EPA"},
+	{"Maryland-DC", "MDC"},
+	{"Western Pennsylvania", "WPA"},
+	{"Alabama", "AL"},
+	{"Georgia", "GA"},
+	{"Kentucky", "KY"},
+	{"North Carolina", "NC"},
+	{"Northern Florida", "NFL"},
+	{"Puerto Rico", "PR"},
+	{"South Carolina", "SC"},
+	{"Southern Florida", "SFL"},
+	{"Tennessee", "TN"},
+	{"Virginia", "VA"},
+	{"US Virgin Islands", "VI"},
+	{"West Central Florida", "WCF"},
+	{"Arkansas", "AR"},
+	{"Louisiana", "LA"},
+	{"Mississippi", "MS"},
+	{"New Mexico", "NM"},
+	{"North Texas", "NTX"},
+	{"Oklahoma", "OK"},
+	{"South Texas", "STX"},
+	{"West Texas", "WTX"},
+	{"East Bay", "EB"},
+	{"Los Angeles", "LAX"},
+	{"Orange", "ORG"},
+	{"Pacific", "PAC"},
+	{"Santa Barbara", "SB"},
+	{"Santa Clara Valley", "SCV"},
+	{"San Diego", "SDG"},
+	{"San Francisco", "SF"},
+	{"San Joaquin Valley", "SJV"},
+	{"Sacramento Valley", "SV"},
+	{"Alaska", "AK"},
+	{"Arizona", "AZ"},
+	{"Eastern Washington", "EWA"},
+	{"Idaho", "ID"},
+	{"Montana", "MT"},
+	{"Nevada", "NV"},
+	{"Oregon", "OR"},
+	{"Utah", "UT"},
+	{"Western Washington", "WWA"},
+	{"Wyoming", "WY"},
+	{"Michigan", "MI"},
+	{"Ohio", "OH"},
+	{"West Virginia", "WV"},
+	{"Illinois", "IL"},
+	{"Indiana", "IN"},
+	{"Wisconsin", "WI"},
+	{"Alberta", "AB"},
+	{"British Columbia", "BC"},
+	{"Ontario Golden Horseshoe", "GH"},
+	{"Manitoba", "MB"},
+	{"New Brunswick", "NB"},
+	{"Newfoundland and Labrador", "NL"},
+	{"Nova Scotia", "NS"},
+	{"Ontario East", "ONE"},
+	{"Ontario North", "ONN"},
+	{"Ontario South", "ONS"},
+	{"Prince Edward Island", "PE"},
+	{"Quebec", "QC"},
+	{"Saskatchewan", "SK"},
+	{"Other", "DX"},
 }
